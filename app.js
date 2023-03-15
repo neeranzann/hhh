ed49e146b205cdd203b7e1e78c59b5703b3a5dee
neeranzann <124778454+neeranzann@users.noreply.github.com>
2023-03-15 17:50:05 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import React from './components/header'
+import Home from './pages/Home'
+
+const Home = () => {
+  return (
+    <div>
+        <header/>
+      <Home/>
+    </div>
+  )
+}
+
+export default app
+
